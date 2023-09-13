d1edd7c424a3102cb1f9819a179c27f91e67488e
edsonsbj <60072822+edsonsbj@users.noreply.github.com>
2023-09-12 22:03:26 -0300

    Create custom.config.php

---
+++ dietpi/custom.config.php
@@ -0,0 +1,29 @@
+<?php
+\$CONFIG = array (
+  'default_phone_region' => 'BR',
+  'skeletondirectory' => '',
+  'enabledPreviewProviders' =>
+  array (
+    0 => 'OC\\Preview\\PNG',
+    1 => 'OC\\Preview\\JPEG',
+    2 => 'OC\\Preview\\GIF',
+    3 => 'OC\\Preview\\BMP',
+    4 => 'OC\\Preview\\XBitmap',
+    5 => 'OC\\Preview\\Movie',
+    6 => 'OC\\Preview\\PDF',
+    7 => 'OC\\Preview\\MP3',
+    8 => 'OC\\Preview\\TXT',
+    9 => 'OC\\Preview\\MarkDown',
+    10 => 'OC\\Preview\\Image',
+    11 => 'OC\\Preview\\HEIC',
+    12 => 'OC\\Preview\\TIFF',
+  ),
+  'trashbin_retention_obligation' => 'auto,30',
+  'versions_retention_obligation' => 'auto,30',
+  'default_language' => 'pt',
+  'force_language' => 'pt',
+  'default_locale' => 'pt_BR',
+  'force_locale' => 'pt_BR',
+  'overwritehost' => '${first_domain}:8443',
+  'overwriteprotocol' => 'https',
+);
